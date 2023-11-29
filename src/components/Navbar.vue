ff41d0bdfe17b4797329762077a60eeed1e9f92f
Jan Kadlec <jan.kadlec@agentes-it.com>
2023-11-29 17:02:46 +0100

    navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,13 +10,13 @@
         <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
           <li><a href="/#about">About</a></li>
           <li>
-            <a href="/#tickets">Tickets</a>
+            <router-link to="/#tickets">Tickets</router-link>
             <ul class="p-2">
-              <li><a href="/#tickets">Standard tickets</a></li>
-              <li><a href="/#tickets_gala">Gala tickets</a></li>
+              <li><router-link to="/#tickets">Standard tickets</router-link></li>
+              <li><router-link to="/#tickets_gala">Gala tickets</router-link></li>
             </ul>
           </li>
-          <li><a href="/#contact">Contact</a></li>
+          <li><router-link to="/#contact">Contact</router-link></li>
         </ul>
       </div>
       <a href="#" class="btn btn-ghost"><img class="h-full px-0" src="/favicon.svg"></a>
