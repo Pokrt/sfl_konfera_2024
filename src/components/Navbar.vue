c297f486bb807c2bdeac93b76307a303f22e0c00
Jan Kadlec <jan.kadlec@agentes-it.com>
2023-11-29 17:56:16 +0100

    navigation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="z-10 navbar bg-base-100  fixed ">
     <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost ">
+      <details  class="dropdown">
+        <summary tabindex="0" role="button" class="btn btn-ghost ">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"/>
           </svg>
-        </div>
+        </summary>
         <ul class="menu menu-sm dropdown-content mt-3 z-20 p-2 shadow bg-base-100 rounded-box w-52">
           <li class="z-30"><router-link to="./#about">Aboutt</router-link></li>
           <li class="z-30"><a href="./#about">Abouttt</a></li>
@@ -20,7 +20,7 @@
           </li>
           <li><router-link to="/#contact">Contact</router-link></li>
         </ul>
-      </div>
+      </details >
       <a href="#" class="btn btn-ghost"><img class="h-full px-0" src="/favicon.svg"></a>
     </div>
     <div class="navbar-center  lg:flex  ">
