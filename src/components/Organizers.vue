c05bb2cb9fca28a65dd00574776497ddbc5dee3c
Jan Kadlec <jan.kadlec@agentes-it.com>
2024-01-03 15:15:34 +0100

    navigation biigerl + soc. links + venues

--- src/components/Organizers.vue
+++ src/components/Organizers.vue
@@ -8,7 +8,7 @@
 
       <div class="card card-side  bg-base-300">
         <figure>
-              <img class="h-48 w-36"  src="https://annamiluje.cz/wp-content/uploads/2020/09/150DSCF1906-edited-768x558.jpg"/>
+          <img class="h-48 w-36" src="https://annamiluje.cz/wp-content/uploads/2020/09/150DSCF1906-edited-768x558.jpg"/>
         </figure>
         <div class="card-body  w-4/12 lg:w-60">
           <h2 class="card-title">Anna Soukup</h2>
@@ -17,17 +17,25 @@
 
       <div class="card card-side bg-base-300">
         <figure>
-              <img class="h-48 w-36"  src="/jd.jpeg" />
+          <img class="h-48 w-36" src="/jd.jpeg"/>
         </figure>
         <div class="card-body w-4/12 lg:w-60">
           <h2 class="card-title">Jana Daniela Šabatková</h2>
+        </div>
+      </div>
+      <div class="card card-side bg-base-300">
+        <figure>
+          <img class="h-48 w-36" src="/hanka.jpeg"/>
+        </figure>
+        <div class="card-body w-4/12 lg:w-60">
+          <h2 class="card-title">Hana Křenková</h2>
         </div>
       </div>
 
 
       <div class="card card-side bg-base-300 ">
         <figure>
-              <img class="h-48 w-36"   src="/andrejka.jpeg" />
+          <img class="h-48 w-36" src="/andrejka.jpeg"/>
         </figure>
         <div class="card-body  w-4/12 lg:w-60">
           <h2 class="card-title">Andrea Bílá</h2>
@@ -36,21 +44,48 @@
 
       <div class="card card-side bg-base-300 ">
         <figure>
-              <img class="h-48 w-36"   src="/jana.jpeg" />
+          <img class="h-48 w-36" src="/jana.jpeg"/>
         </figure>
         <div class="card-body  w-4/12 lg:w-60">
           <h2 class="card-title">Jana Havlíčková</h2>
         </div>
       </div>
+
       <div class="card card-side bg-base-300 ">
         <figure>
-              <img class="h-48 w-36"   src="/honza.jpeg" />
+          <img class="h-48 w-36" src="/honza.jpeg"/>
         </figure>
         <div class="card-body  w-4/12 lg:w-60">
           <h2 class="card-title">Jan Kadlec</h2>
         </div>
       </div>
 
+      <div class="card card-side bg-base-300 ">
+        <figure>
+          <img class="h-48 w-36" src="/klara.jpeg"/>
+        </figure>
+        <div class="card-body  w-4/12 lg:w-60">
+          <h2 class="card-title">Klára Zezulová</h2>
+        </div>
+      </div>
+
+      <div class="card card-side bg-base-300 ">
+        <figure>
+          <img class="h-48 w-36" src="/adela.jpeg"/>
+        </figure>
+        <div class="card-body  w-4/12 lg:w-60">
+          <h2 class="card-title">Adéla Kristýna Vaňková</h2>
+        </div>
+      </div>
+
+      <div class="card card-side bg-base-300 ">
+        <figure>
+          <img class="h-48 w-36" src="/roman.jpg"/>
+        </figure>
+        <div class="card-body  w-4/12 lg:w-60">
+          <h2 class="card-title">Roman Jasenovec</h2>
+        </div>
+      </div>
 
 
     </div>
