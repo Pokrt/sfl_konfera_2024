b345941bd9e958e5561d2f12e1fabf3d6ce7bcd9
Jan Kadlec <jan.kadlec@agentes-it.com>
2024-02-04 14:39:59 +0100

    update

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -89,6 +89,22 @@
       </div>
 
 
+      <div class="card w-full lg:w-96 bg-base-300 p-10  grid justify-items-center  card-bordered  ">
+        <figure class="lg:mt-5 w-7/12 "><img src="/CzechInvest.svg" alt="CzechInvest"/></figure>
+        <div class="card-actions justify-end mt-10 self-end border-primary">
+          <a href="https://www.czechinvest.org/en"
+             class="btn btn-outline btn-neutral btn-square flex align-middle ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="h-6 w-6"
+                 stroke="currentColor">
+              <path
+                  d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z"
+                  fill="currentColor"/>
+            </svg>
+          </a>
+        </div>
+      </div>
+
+
     </div>
   </div>
 
