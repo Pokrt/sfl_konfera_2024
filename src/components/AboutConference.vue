66cb4bceeaad6cdf240fdd46f2ce3c7ed2a952a8
Stepan Kovar <kovar.stepan@protonmail.com>
2024-10-05 19:26:57 +0200

    introduce About Conference

---
+++ src/components/AboutConference.vue
@@ -0,0 +1,27 @@
+<template>
+  <section id="about-conference" class="w-full max-w-6xl mx-auto py-12 md:py-24 px-4 md:px-6">
+    <div class="my-16 flex place-content-center lg:mx-auto mx-5 text-justify">
+      <div class="prose lg:prose-xl text-black prose-headings:text-neutral">
+        <h1 id="about">O konferenci</h1>
+      </div>
+    </div>
+    <div class="text-black text-lg md:text-xl text-justify">
+      <p>
+        Dne 26. října se v Praze uskuteční konference Legalizujme, pořádaná <a href="https://studentsforlibertycz.cz/" target="_blank" class="text-blue-600 underline">Students for Liberty CZ</a>. Cílem konference je představit reálné alternativy k současné prohibiční drogové politice a nabídnout konkrétní řešení pro její zlepšení.
+      </p>
+      <p class="mt-4">
+        Součástí konference budou diskuzní panely zaměřené například na oběti represe, legalizaci konopí nebo harm reduction přístup. Formální část začíná ve 13:00 a končí v 18:00, následovat bude neformální networking a afterparty. Celá akce proběhne v <a href="https://www.kulturnisportovna.cz/" target="_blank" class="text-blue-600 underline">Radlické kulturní sportovně</a>, kde bude k dispozici také bar s občerstvením.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AboutConference",
+};
+</script>
+
+<style scoped>
+/* Add your specific styles here if needed */
+</style>
